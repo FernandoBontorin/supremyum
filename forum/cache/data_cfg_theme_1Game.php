b3d58b2cf360f65eb292801e8810a21390c47cfe
Fernando Bontorin <fe_bon@hotmail.com>
2019-07-27 00:09:33 -0300

    mini cleanup

--- forum/cache/data_cfg_theme_1Game.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1397675838
-162
-a:5:{s:4:"name";s:5:"1Game";s:9:"copyright";s:29:"&copy; 2010 www.GamesBoard.pl";s:7:"version";s:5:"3.0.7";s:14:"parse_css_file";b:0;s:8:"filetime";i:1267656120;}
